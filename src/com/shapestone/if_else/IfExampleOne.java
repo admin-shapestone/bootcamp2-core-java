337eaeb3bb27ae7e30cb2a67aaa2035a3d3b6093
phanirama <phanirama.shapestone@gmail.com>
2023-05-16 11:59:52 +0530

    if else examples

---
+++ src/com/shapestone/if_else/IfExampleOne.java
@@ -0,0 +1,9 @@
+public class IfExampleOne {
+    public static void main(String args[]) {
+    int num=70;
+    if( num < 100 ) {
+    System.out.println("number is greater than 50");
+    
+    }
+  }
+ }

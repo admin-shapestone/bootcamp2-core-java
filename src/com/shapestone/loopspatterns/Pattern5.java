bab2eab168ac1c88dca36db42597f210c92366ca
abhilashdumma <abhilash.shapestone.com>
2023-05-16 22:43:29 +0530

    patterns

---
+++ src/com/shapestone/loopspatterns/Pattern5.java
@@ -0,0 +1,20 @@
+class Pattern5
+ {
+ public static void main(String[] args)
+ {
+ 
+for(char i='A';i<='D';i++)
+{
+ for(char j='A';j<='D';j++)
+{
+ System.out.print(j+" ");
+ }
+ System.out.println();
+ }
+}
+}
+ 
+//A B C D
+//A B C D
+//A B C D
+//A B C D
---
+++ src/com/shapestone/loopspatterns/Pattern9.java
@@ -0,0 +1,7 @@
+class Pattern9
+{
+ public static void main(String[] args)
+  {
+   for(int i=1;i<=4;i++)
+  {
+   for(int j=1;j<

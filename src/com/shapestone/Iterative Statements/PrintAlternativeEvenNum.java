d1f7c52c6bae7aa674f0e3b31e7e573838593391
abhilashdumma <abhilash.shapestone.com>
2023-05-16 22:39:02 +0530

    Iterative statements

---
+++ src/com/shapestone/Iterative Statements/PrintAlternativeEvenNum.java
@@ -0,0 +1,12 @@
+class PrintAlternativeEvenNum
+{
+ public static void main(String[] args)
+ {
+   for(int i=2;i<=100;i=i+4)
+   {  
+     System.out.print(i+" ");
+
+   }
+ }
+}
+      
---
+++ src/com/shapestone/Iterative Statements/PrintEvenNumUpto100.java
@@ -0,0 +1,15 @@
+class PrintEvenNumUpto100
+{
+ public static void main(String []args)
+{
+   int num = 1;
+   while(num<100)
+ {
+   if(num%2==0)
+  {
+   System.out.print(num+" ");
+  }
+   num++;
+ }
+}
+}

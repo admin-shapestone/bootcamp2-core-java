ff8d0451c0ce97bd29e6f5bb3fc1174f9a57c52c
Nitish <nitishrayapati.shapestone@gmail.com>
2023-05-17 15:40:45 +0530

    Flow Controls

---
+++ src/flowcontrols/ForExample.java
@@ -0,0 +1,13 @@
+package com.shapestone.flowcontrols;
+
+public class ForExample {
+
+	
+	public static void main(String[] args) {
+		
+		
+		for (int start = 1; start <=100; start++) {
+			System.out.println(start);
+		}
+	}
+}

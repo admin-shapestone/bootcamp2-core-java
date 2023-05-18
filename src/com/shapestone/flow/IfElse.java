5750155b05820d4cef05a08b336ef0c195161589
shapestone <luckyabhilash1234@gmail.com>
2023-05-18 15:09:57 +0530

    if else example

---
+++ src/com/shapestone/flow/IfElse.java
@@ -0,0 +1,15 @@
+package com.shapestone.flow;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+			int a=20;
+			if (a%2==0)
+			{
+				System.out.println("it is a even number");
+			}
+			else {
+				System.out.println("it is a odd number");
+			}
+			}
+}

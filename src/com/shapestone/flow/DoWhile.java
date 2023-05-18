6406703a9d35a984e1904ca4c61e3d158802a233
shapestone <luckyabhilash1234@gmail.com>
2023-05-18 12:18:30 +0530

    do while programs

---
+++ src/com/shapestone/flow/DoWhile.java
@@ -0,0 +1,16 @@
+package com.shapestone.flow;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		do
+		{
+		System.out.println("shapestoen");
+		}
+		while(a>b);
+			System.out.println("java class");
+		}
+
+	}
+

10b71371d83883ea23e4112a69f4ae5d85ffa8ce
ADMIN <abhilashdumma.shapestone@gmail.com>
2023-05-19 14:39:02 +0530

    push example for abstarct

---
+++ src/com/shapestone/accessmodifiers/TestForAbstarct.java
@@ -0,0 +1,17 @@
+package com.shapestone.accessmodifiers;
+
+public class  TestForAbstarct
+{
+ public static void main(String[] arg) 
+ {
+	 Bus b = new Bus();
+	 System.out.println(b.NoOfVehicles());
+	 Car c = new Car();
+	 System.out.println(c.NoOfVehicles());
+ }
+	
+	
+	
+	
+
+}

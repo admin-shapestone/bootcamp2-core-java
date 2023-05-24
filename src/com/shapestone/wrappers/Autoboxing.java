6a4d504fe03a7f281fd475a749a6e9878eecae07
GopiK <gopikrishna.shapestone@gmail.com>
2023-05-24 11:36:56 +0530

    Auto boxing example

---
+++ src/com/shapestone/wrappers/Autoboxing.java
@@ -0,0 +1,16 @@
+package com.shapestone.wrappers;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		int a = 50;
+		Integer a2 = new Integer(a);
+		
+		 Integer a3 = 5;
+		 System.out.println(a2+" "+a3);
+		 
+				 
+		
+	}
+
+}

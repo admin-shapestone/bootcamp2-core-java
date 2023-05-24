e847af4ee0a31b7f7a4edb62be6a091e3a4182e7
phanirama <phanirama.shapestone@gmail.com>
2023-05-24 15:04:21 +0530

    default constructor example

---
+++ src/com/shapestone/constructors/DefaultConstructorExample.java
@@ -0,0 +1,12 @@
+package com.shapestone.constructors;
+
+public class DefaultConstructorExample {
+	DefaultConstructorExample()
+	{
+		System.out.println("Default Constructor Example");
+	}
+	public static void main(String args[]) {
+		DefaultConstructorExample old = new DefaultConstructorExample();
+	}
+
+}

4c6c9e7b7cbabdf1916604f658ab6514902e04d0
GopiK <gopikrishna.shapestone@gmail.com>
2023-05-24 11:44:47 +0530

    unboxing example

---
+++ src/com/shapestone/wrappers/Unboxing.java
@@ -0,0 +1,12 @@
+package com.shapestone.wrappers;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+		Integer i = new Integer (50);
+		int a = i;
+		System.out.println(a);
+
+	}
+
+}

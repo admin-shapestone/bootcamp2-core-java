0562ad3823e32f8abca473ef8799bd7d016aef90
GopiK <gopikrishna.shapestone@gmail.com>
2023-05-24 14:54:56 +0530

    Default constructor

---
+++ src/com/shapestone/constructor/Defaultconstructor.java
@@ -0,0 +1,15 @@
+package com.shapestone.constructor;
+
+public class Defaultconstructor {
+	
+	Defaultconstructor () 
+	{
+		System.out.println("Defaultconstructor is created");
+		}
+	
+	public static void main(String[] args) {
+		Defaultconstructor b = new Defaultconstructor ();
+
+	}
+
+}

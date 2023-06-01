4eab52e0526637e0045e3c1057353464e7c3603f
nages <nagesh.shapestone@gmail.com>
2023-06-01 22:57:15 +0530

    numbers examples

---
+++ src/com/shapestone/assignments/NumbersExample.java
@@ -0,0 +1,13 @@
+package com.shapestone.assignments;
+
+public class NumbersExample {
+
+	public static void main(String[] args) {
+		
+		for (int count =0; count<=50; count++) {
+			System.out.println(count);
+		}
+
+	}
+
+}

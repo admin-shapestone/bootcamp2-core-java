703d451170c9b5b9de4c86354688eda4b447cd2a
phanirama <phanirama.shapestone@gmail.com>
2023-06-02 01:06:13 +0530

    inheritance

---
+++ src/com/shapestone/inheritance/Admin.java
@@ -0,0 +1,10 @@
+package com.shapestone.inheritance;
+
+public class Admin extends Developer {
+
+	// Adding or deleting products or applications
+	public void manage() {
+		System.out.println("Manage code");
+	}
+
+}
---
+++ src/com/shapestone/inheritance/Developer.java
@@ -0,0 +1,7 @@
+package com.shapestone.inheritance;
+
+public class Developer extends Guest {
+	public void write() {
+		System.out.println("write code");
+	}
+}

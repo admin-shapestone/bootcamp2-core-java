298ca81afba7d1e0e24723c73cbac3b14289cd56
nages <nagesh.shapestone@gmail.com>
2023-06-03 06:49:14 +0530

    OVERLOADING

--- src/com/shapestone/overloading/Calculations.java
+++ src/com/shapestone/overloading/Calculations.java
@@ -1,5 +1,21 @@
 package com.shapestone.overloading;
 
 public class Calculations {
+	
+	public void add1(int a,int b) {
+		System.out.println("calling method is add1");
+		
+	}
+	
+	public void add1(int a, int b, int c) {
+		System.out.println("calling method is add2");
+		
+	}
+	public static void main(String[] args) {
+		Calculations c = new Calculations();
+		c.add1(5,10);
+		
+	
+	}
 
 }

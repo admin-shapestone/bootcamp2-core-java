8d2fa8b0e65e342841658f0f46fca3e58dabaa73
Nitish <nitishrayapati.shapestone@gmail.com>
2023-06-05 16:00:40 +0530

    fundamental programs

---
+++ src/com/shapestone/fundamentals/Classes3.java
@@ -0,0 +1,20 @@
+package com.shapestone.fundamentals;
+
+class Classes {
+
+	public static void main(String[] args) {
+		System.out.println("Hii...!");
+	}
+}
+
+class Classes2 {
+	public static void main(String[] args) {
+		System.out.println("Hello..");
+	}
+}
+
+public class Classes3 {
+	public static void main(String[] args) {
+		System.out.println("Namastey");
+	}
+}
---
+++ src/com/shapestone/fundamentals/DefaultValues.java
@@ -0,0 +1,25 @@
+package com.shapestone.fundamentals;
+
+public class DefaultValues {
+	int c;
+	boolean h;
+	byte i;
+	short n;
+	double t;
+	char u;
+	float uu;
+	long oii;
+
+	public static void main(String[] args) {
+		DefaultValues obj = new DefaultValues();
+		System.out.println(obj.c);
+		System.out.println(obj.h);
+		System.out.println(obj.i);
+		System.out.println(obj.n);
+		System.out.println(obj.t);
+		System.out.println(obj.u);
+		System.out.println(obj.uu);
+		System.out.println(obj.oii);
+	}
+
+}

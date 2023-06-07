1208d25a50b766a05dfbb5cf9396a6fbf6ccfd1f
nages <nagesh.shapestone@gmail.com>
2023-06-07 11:39:43 +0530

    fundamentals

---
+++ src/com/shapestone/fundamentals/DefaultExample1.java
@@ -0,0 +1,26 @@
+package com.shapestone.fundamentals;
+
+public class DefaultExample1 {
+	int i;
+	byte b;
+	short s;
+	long l;
+	float f;
+	double d;
+	boolean a;
+	char c;
+
+	public static void main(String[] args) {
+		DefaultExample1 obj = new DefaultExample1();
+		System.out.println(obj.i);
+		System.out.println(obj.b);
+		System.out.println(obj.s);
+		System.out.println(obj.l);
+		System.out.println(obj.f);
+		System.out.println(obj.d);
+		System.out.println(obj.a);
+		System.out.println(obj.c);
+
+	}
+
+}
---
+++ src/com/shapestone/fundamentals/HelloJava.java
@@ -0,0 +1,10 @@
+package com.shapestone.fundamentals;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("hellojava....");
+
+	}
+
+}

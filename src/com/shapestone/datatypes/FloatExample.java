74ed375a6cf17b5ad5f828ece51e586b6dfbc06c
Nagesh Vunnam <nagesh.shapestone@gmail.com>
2023-06-08 10:40:28 +0530

    data types

---
+++ src/com/shapestone/datatypes/FloatExample.java
@@ -0,0 +1,11 @@
+package com.shapestone.datatypes;
+
+public class FloatExample {
+
+	public static void main(String[] args) {
+		float f = 5000;
+		System.out.println("account balance :"+f);
+
+	}
+
+}

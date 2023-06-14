dbebe003024b6c5890b7ce7984b0073e30afbbdb
Phanirama <phanirama.shapestone@gmail.com>
2023-06-14 16:53:17 +0530

    conditional statements 13-06-23

---
+++ src/com/shapestone/conditionalstatements/IfCondition.java
@@ -0,0 +1,10 @@
+package com.shapestone.conditionalstatements;
+
+public class IfCondition {
+	public static void main(String[] args) {
+		int number = 10;
+		if (number < 15) {
+			System.out.println("the statement is correct ");
+		}
+	}
+}
---
+++ src/com/shapestone/conditionalstatements/IfElseExampeTwo.java
@@ -0,0 +1,12 @@
+package com.shapestone.conditionalstatements;
+
+public class IfElseExampeTwo {
+	public static void main(String[] args) {
+		int number = 7;
+		if (number % 2 == 0) {
+			System.out.println("The number is even");
+		} else {
+			System.out.println("The number is odd");
+		}
+	}
+}

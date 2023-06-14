84b63d36805fc69ae168356e2de7191540f1fd79
Phanirama <phanirama.shapestone@gmail.com>
2023-06-14 14:28:05 +0530

    iterative statements 13-06-23

---
+++ src/com/shapestone/iterativestatements/ForStatementExample.java
@@ -0,0 +1,9 @@
+package com.shapestone.iterativestatements;
+
+public class ForStatementExample {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("count: " + i);
+		}
+	}
+}

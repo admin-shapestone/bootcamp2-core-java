dfd8fe8cb05435b165288fb43a885ca539508600
GopiK <gopikrishna.shapestone@gmail.com>
2023-06-14 11:41:32 +0530

    exception handling using finally

---
+++ src/com/shapestone/exceptionhandlingtask/ExceptionHandlingOnFinally.java
@@ -0,0 +1,23 @@
+package com.shapestone.exceptionhandlingtask;
+
+public class ExceptionHandlingOnFinally {
+
+	public static void main(String[] args) {
+		try {
+		      
+		      int divideByZero = 5 / 0;
+		    }
+
+		    catch (ArithmeticException e) {
+		      System.out.println("ArithmeticException => " + e.getMessage());
+		    }
+		    
+		    finally {
+		      System.out.println("This is the finally block");
+		    }
+		  }
+		
+
+	}
+
+

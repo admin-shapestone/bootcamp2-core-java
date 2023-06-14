6effaf1f9b38296a8ac732b4d1ef6adcb926ea45
Nitish <nitishrayapati.shapestone@gmail.com>
2023-06-14 16:44:08 +0530

    datatypes 14-06-23

---
+++ src/com/shapestone/alldatatypes/BooleanExample.java
@@ -0,0 +1,15 @@
+package com.shapestone.alldatatypes;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean isRaining = false;
+		if (isRaining) {
+			System.out.println("Remember to take an umbrella!");
+		} else {
+			System.out.println("No need for an umbrella.");
+		}
+
+	}
+
+}

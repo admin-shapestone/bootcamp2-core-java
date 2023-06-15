8ff8dd82f21ea0141f7f54423eb25a5086dc5273
ADMIN <abhilashdumma.shapestone@gmail.com>
2023-06-15 22:07:12 +0530

    psuh read file in json

---
+++ src/com/shapestone/files/Student.java
@@ -0,0 +1,41 @@
+package com.shapestone.files;
+
+public class Student {
+	private String name;
+	private String course;
+	private String phoneNumber;
+	private Address address;
+
+	public String getName() {
+		return name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}

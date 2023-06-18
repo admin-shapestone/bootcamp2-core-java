bb8a0fc4b9628a72d885e40f926d1862b074f0e7
GopiK <gopikrishna.shapestone@gmail.com>
2023-06-18 22:35:08 +0530

    collection examples

---
+++ src/com/shapestone/collectionpractice/LinkedListExample.java
@@ -0,0 +1,19 @@
+package com.shapestone.collectionpractice;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+		LinkedList<String>ll=new LinkedList<>();
+		ll.add("car");
+		ll.add("Bike");
+		ll.add("Bus");
+		ll.add(1,"Super car");
+		System.out.println(ll);
+			
+		
+
+	}
+
+}
---
+++ src/com/shapestone/collectionpractice/VectorExample.java
@@ -0,0 +1,17 @@
+package com.shapestone.collectionpractice;
+
+import java.util.Vector;
+
+public class VectorExample {
+
+	public static void main(String[] args) {
+		Vector V = new Vector();
+		 V.add ("gopi");
+		 V.add ("nitish");
+		 V.add("nagesh");
+		 V.add(0,"phani");
+		 System.out.println(V);
+
+	}
+
+}

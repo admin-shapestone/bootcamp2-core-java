12154bc6f8abd9777e71cf91b9017e175a1944b7
shapestone <luckyabhilash1234@gmail.com>
2023-06-22 20:37:11 +0530

    caluculating the total amount of patient

--- src/com/shapestone/patientdetails/PatientMain.java
+++ src/com/shapestone/patientdetails/PatientMain.java
@@ -1,22 +1,19 @@
 package com.shapestone.patientdetails;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PatientMain {
 
 	public static void main(String[] args) throws StreamReadException, DatabindException, IOException {
-		int totalamount = 1;
+
 		ObjectMapper om = new ObjectMapper();
-		PatientPojo[] arr = om.readValue(new java.io.File(
-				"C:\\Users\\91901\\projects\\bootcamp2-core-java\\src\\com\\shapestone\\patientdetails\\patients.json"),
-				PatientPojo[].class);
-		TreatmentPojo[] trr = om.readValue(new java.io.File(
-				"C:\\Users\\91901\\projects\\bootcamp2-core-java\\src\\com\\shapestone\\patientdetails\\tratement.json"),
-				TreatmentPojo[].class);
 
 		System.out.println(
 				"-------------------------------------------------------------------------------PATIENTS DETAILS---------------------------------------------------------------------------------------------------------------");
@@ -28,16 +25,58 @@ public class PatientMain {
 		System.out.println(
 				"-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
 
-		for (int i = 0; i < arr.length; i++) {
-			int totalAmount = trr[i].getScanningBill() + trr[i].getMedicines() + trr[i].getDoctorfee();
-			System.out.printf("|%15s|%15s|%10s|%10s|%15s|%15s|%15s|%15s|%15s|%15s|%15s|%15s|%15s|%n",
-					trr[i].getSerialno(), arr[i].getName(), arr[i].getAge(), arr[i].getGender(), arr[i].getId(),
-					arr[i].getDateofadmission(), trr[i].getTreatment(), trr[i].getDiagnosis(),
-					trr[i].getDateoftreatment(), trr[i].getDoctorfee(), trr[i].getScanningBill(), trr[i].getMedicines(),
-					totalAmount);
-		}
+		ArrayList<PatientP> patientList = om.readValue(new java.io.File(
+				"C:\\Users\\91901\\projects\\bootcamp2-core-java\\src\\com\\shapestone\\patientdetails\\patients.json"),
+				new TypeReference<ArrayList<PatientP>>() {
+				});
+		ArrayList<TreatmentP> treatmentList = om.readValue(new java.io.File(
+				"C:\\Users\\91901\\projects\\bootcamp2-core-java\\src\\com\\shapestone\\patientdetails\\tratement.json"),
+				new TypeReference<ArrayList<TreatmentP>>() {
+				});
 
+		for (int i = 0; i < patientList.size(); i++) {
+			PatientP patient = patientList.get(i);
+			TreatmentP treatment = treatmentList.get(i);
+			int totalAmount = treatment.getScanningBill() + treatment.getMedicines() + treatment.getDoctorfee();
+			System.out.printf("|%15s|%15s|%10s|%10s|%15s|%15s|%15s|%15s|%15s|%15s|%15s|%15s|%15s|%n",
+					treatment.getSerialno(), patient.getName(), patient.getAge(), patient.getGender(), patient.getId(),
+					patient.getDateofadmission(), treatment.getTreatment(), treatment.getDiagnosis(),
+					treatment.getDateoftreatment(), treatment.getDoctorfee(), treatment.getScanningBill(),
+					treatment.getMedicines(), totalAmount);
+		}
 		System.out.println(
-				"-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-	}
-}
+				"--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+	       Scanner sc=new Scanner(System.in);
+	       System.out.println("enter the id number");
+	       String id=sc.nextLine();
+	       for(int i=0;i<patientList.size();i++)
+	       {
+	       PatientP patient = patientList.get(i);
+		   TreatmentP treatment = treatmentList.get(i);
+           if( patient.getId().equals(id)){
+	       System.out.println(patient.toString());
+	       System.out.println(treatment.toString());
+	       return; 
+           }}
+	     //  else
+	       {
+		       System.out.println("invalid patient id");}
+
+	       
+	
+	       }
+	       }   
+	
+	       
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+

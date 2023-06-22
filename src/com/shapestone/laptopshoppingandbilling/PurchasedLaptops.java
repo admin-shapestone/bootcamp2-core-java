acd78677aae88a2812a821ecd2670ebef664878e
Phanirama <phanirama.shapestone@gmail.com>
2023-06-22 15:00:09 +0530

    total purchased laptops today

--- src/com/shapestone/laptopshoppingandbilling/PurchasedLaptops.java
+++ src/com/shapestone/laptopshoppingandbilling/PurchasedLaptops.java
@@ -11,33 +11,20 @@ public class PurchasedLaptops {
 		ObjectMapper purchase = new ObjectMapper();
 		Customers[] customer = purchase.readValue(new java.io.File("customers.json"), Customers[].class);
 		Purchases[] today = purchase.readValue(new java.io.File("purchases.json"), Purchases[].class);
-          String todayPurchase ="2023-06-21";
-		for (int i = 0; i <= customer.length - 1; i++) {
-			if(todayPurchase.equals(customer[i].getDateOfPurchase()))
-			{
-			  System.out.println("Customer ID         : " + customer[i].getCustomerId());
-			  System.out.println("Name                : " + customer[i].getName());
-			  System.out.println("Age                 : " + customer[i].getAge());
-			  System.out.println("Gender              : " + customer[i].getGender());
-			  System.out.println("Date Of Purchase: " + customer[i].getDateOfPurchase());
+		String todayPurchase = "2023-06-21";
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------------------------------------");
+		System.out.printf("| %-17s | %-17s | %-17s | %-17s | %-17s | %-15s | %-9s |%n", "Customer ID", "Name", "Age", "Gender",
+				"Date Of Purchase", "Product ", "Price ");
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------------------------------------");
+		for (int i = 0; i <= customer.length - 1; i++)
+			if (todayPurchase.equals(customer[i].getDateOfPurchase())) {
+				System.out.printf("| %-17s | %-17s | %-17s | %-17s | %-17s | %-15s | %-9s |%n", customer[i].getCustomerId(),
+						customer[i].getName(), customer[i].getAge(), customer[i].getGender(),
+						customer[i].getDateOfPurchase(),today[i].getProduct(),today[i].getPrice());
+
 			}
-			//System.out.println("--------------------------------------------------------");
-		
-		}
-	
-		  for (int j = 0; j < today.length - 1; j++) {
-			  if(todayPurchase.equals(customer[j].getDateOfPurchase())) 
-			  {
-				  System.out.println("Purchased ID: " + today[j].getPurchaseId());
-				  System.out.println("Customer ID : " + today[j].getCustomerId());
-				  System.out.println("Product     : " + today[j].getProduct());
-				  System.out.println("Price       : " + today[j].getPrice());
-			  }
-		 // System.out.println("--------------------------------------------------------" );
-		  
-		  }
-		 
-			  
-	
+
 	}
 }

eb24af750eb6abbac4e23dd9bdacc0c3b01048cb
nages <nagesh.shapestone@gmail.com>
2023-06-26 16:31:58 +0530

    studentmanagement

--- src/com/shapestone/studentmanagement/CertificateDoneByToday.java
+++ src/com/shapestone/studentmanagement/CertificateDoneByToday.java
@@ -5,22 +5,21 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class CertificateDoneByToday {
-	 public void Certificates(ArrayList<Certificates> certificatesList) {
-	System.out.println(
-			"---------------------------------------------------------------------------------------------------------|");
-	System.out.printf("|%-15s %-15s %-15s|%-15s| %-15s | %-15s |%n", "Student ID", "Student Name",
-			"StudentGender", "StudentAge", "StudentDateOfJoining", "StudentAddress");
-	System.out.println(
-			"----------------------------------------------------------------------------------------------------------|");
-	
+	public void Certificates(ArrayList<Certificates> certificatesList) {
+		System.out.println(
+				"---------------------------------------------------------------------------------------------------------|");
+		System.out.printf("|%-15s %-15s %-15s|%-15s| %-15s | %-15s |%n", "Student ID", "Student Name", "StudentGender",
+				"StudentAge", "StudentDateOfJoining", "StudentAddress");
+		System.out.println(
+				"----------------------------------------------------------------------------------------------------------|");
 
-	System.out.println(
-			"---------------------------------------------------------------------------------------------------------|");
-	System.out.printf("|%-15s %-15s %-15s|%-15s| %-15s | %-15s |%n", "Student ID", "Student Name",
-			"StudentGender", "StudentAge", "StudentDateOfJoining", "StudentAddress");
-	System.out.println(
-			"---------------------------------------------------------------------------------------------------------|");
+		System.out.println(
+				"---------------------------------------------------------------------------------------------------------|");
+		System.out.printf("|%-15s %-15s %-15s|%-15s| %-15s | %-15s |%n", "Student ID", "Student Name", "StudentGender",
+				"StudentAge", "StudentDateOfJoining", "StudentAddress");
+		System.out.println(
+				"---------------------------------------------------------------------------------------------------------|");
 
-}
+	}
 
 }
--- src/com/shapestone/studentmanagement/Certificates.java
+++ src/com/shapestone/studentmanagement/Certificates.java
@@ -6,41 +6,51 @@ public class Certificates {
 	private String certification;
 	private float fees;
 	private String DateOfCertificationDone;
+
 	public int getSlno() {
 		return slno;
 	}
+
 	public void setSlno(int slno) {
 		this.slno = slno;
 	}
+
 	public int getStudentId() {
 		return studentId;
 	}
+
 	public void setStudentId(int studentId) {
 		this.studentId = studentId;
 	}
+
 	public String getCertification() {
 		return certification;
 	}
+
 	public void setCertification(String certification) {
 		this.certification = certification;
 	}
+
 	public float getFees() {
 		return fees;
 	}
+
 	public void setFees(float fees) {
 		this.fees = fees;
 	}
+
 	public String getDateOfCertificationDone() {
 		return DateOfCertificationDone;
 	}
+
 	public void setDateOfCertificationDone(String dateOfCertificationDone) {
 		DateOfCertificationDone = dateOfCertificationDone;
 	}
+
 	@Override
 	public String toString() {
 		return "Certificates [slno=" + slno + ", studentId=" + studentId + ", certification=" + certification
 				+ ", fees=" + fees + ", DateOfCertificationDone=" + DateOfCertificationDone + "]";
 	}
-	
 
 }

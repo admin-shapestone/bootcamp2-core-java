bfe8389db65776f739dab23da021db26c5e56270
ADMIN <abhilashdumma.shapestone@gmail.com>
2023-06-26 14:55:02 +0530

    comit changes

--- src/com/shapestone/banking_domian/Payments.java
+++ src/com/shapestone/banking_domian/Payments.java
@@ -8,42 +8,53 @@ public class Payments {
 	private long amountPaid;
 	private long amountRecieved;
 	private String dateOfPayment;
+
 	public int getSlNo() {
 		return slNo;
 	}
+
 	public void setSlNo(int slNo) {
 		this.slNo = slNo;
 	}
+
 	public long getAccountId() {
 		return accountId;
 	}
+
 	public void setAccountId(long accountId) {
 		this.accountId = accountId;
 	}
+
 	public String getPurposeOfPayment() {
 		return purposeOfPayment;
 	}
+
 	public void setPurposeOfPayment(String purposeOfPayment) {
 		this.purposeOfPayment = purposeOfPayment;
 	}
+
 	public long getAmountPaid() {
 		return amountPaid;
 	}
+
 	public void setAmountPaid(long amountPaid) {
 		this.amountPaid = amountPaid;
 	}
+
 	public long getAmountRecieved() {
 		return amountRecieved;
 	}
+
 	public void setAmountRecieved(long amountRecieved) {
 		this.amountRecieved = amountRecieved;
 	}
+
 	public String getDateOfPayment() {
 		return dateOfPayment;
 	}
+
 	public void setDateOfPayment(String dateOfPayment) {
 		this.dateOfPayment = dateOfPayment;
 	}
-	
-	
+
 }
